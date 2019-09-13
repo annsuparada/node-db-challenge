ae77e1a064add0a3050127fcbfc190b3e77703c7
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-13 14:53:15 -0500

    fix GET tasks

--- tasks/taskHelper.js
+++ tasks/taskHelper.js
@@ -7,20 +7,12 @@ module.exports = {
 }
 
 function getTasks() {
-    return db('projects as p')
-        .join('project_task as pt', 'p.id', 'pt.project_id' )
-        .join('tasks as t', 't.id', 'pt.task_id')
-        .select('p.project_name', 
-                'p.description as project_description', 
-                't.description as task',
-                't.note',
-                't.completed')
+    return db('tasks')
     }
 function getTaskById(id) {
     return db('tasks as t')
-    .join('project_task as pt', 'pt.project_id', 't.id')
-    .join('projects as p', 'p.id', 'pt.project_id')
-    .select('t.id', 't.description', 'p.name as project_name', 'p.description as project_description', 't.completed')
+    .join('projects as p', 'p.id', '=', 't.project_id')
+    .select('t.id', 't.description', 'p.project_name', 'p.description as project_description', 't.completed')
     .where({'t.id': id})
     .first()
         .then(response=> {
