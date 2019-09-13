6a5559356df3d08c588df033096445fb799195a3
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-13 13:23:01 -0500

    POST projects, resouces

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', validateProject, (req, res) => {
+    const project = req.body;
+    db.addProjects(project)
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+            .json({errorMessage: 'There was an error while saving the project to the database'})
+        })
+})
+
+
+function validateProject(req, res, next) {
+    if(!req.body) res.status(400).json({ message: "missing project data" })
+    if(!req.body.project_name) res.status(400).json({ message: "missing required project name field" })
+    next()
+};
 module.exports = router;
--- resouces/resouceRouter.js
+++ resouces/resouceRouter.js
@@ -12,5 +12,23 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: 'Failed to get resouces' })
         })
 })
+router.post('/', validateResouce, (req, res) => {
+    const resouce = req.body;
+    db.addResouce(resouce)
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+            .json({errorMessage: 'There was an error while saving the resouce to the database'})
+        })
+})
 
+
+function validateResouce(req, res, next) {
+    if(!req.body) res.status(400).json({ message: "missing resouce data" })
+    if(!req.body.resouce_name) res.status(400).json({ message: "missing required resouce name field" })
+    next()
+};
 module.exports = router;
